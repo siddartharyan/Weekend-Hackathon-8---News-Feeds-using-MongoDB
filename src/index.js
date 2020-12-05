08f1d9096e84cbfb988c320944e509ed1ae04f61
unknown <siddartharyan180@gmail.com>
2020-12-05 18:34:38 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -3,72 +3,54 @@ const app = express()
 const port = 8080
 
 const onePageArticleCount = 10
-let data = require('./data');
-data = data['data'];
+const { newsArticleSchema } = require('./schema')
+const { newsArticleModel } = require('./connector');
+const News = newsArticleModel;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.get('/newsFeeds', (req, res) => {
+const getAns = (data, limit, offset) => {
     let arr = [];
     for (let i = 0; i < 10; i++) {
         arr.push(data[i]);
     }
-    res.json(arr);
+    return arr;
+}
+app.get('/newsFeeds', (req, res) => {
+    News.find({}).then((result) => res.json(getAns(result, 10, 0)))
 })
 
 app.get('/newsFeeds/:limit/:offset', (req, res) => {
     let limit = req.params.limit;
     let offset = req.params.limit;
-    let arr = [];
     if ((isNaN(limit) || isNaN(offset)) || (Number(limit) >= data.length || Number(offset) >= data.length)) {
-        for (let i = 0; i < 10; i++) {
-            arr.push(data[i]);
-        }
-        res.json(arr);
+        News.find({}).then((result) => res.json(getAns(result, 10, 0)));
         return;
     }
     offset = Number(offset);
     limit = Number(limit);
-    for (let i = offset; i < offset + limit; i++) {
-        arr.push(data[i]);
-    }
-    res.json(arr);
+    News.find({}).then((result) => res.json(getAns(result, limit, offset)));
 })
 
 app.get('/newsFeeds/:limit', (req, res) => {
     let limit = req.params.limit;
-    let arr = [];
     if (isNaN(limit) || Number(limit) >= data.length) {
-        for (let i = 0; i < 10; i++) {
-            arr.push(data[i]);
-        }
-        res.json(arr);
-        return;
+        News.find({}).then((result) => res.json(getAns(result, 10, 0)));
     }
     limit = Number(limit);
-    for (let i = 0; i < limit; i++) {
-        arr.push(data[i]);
-    }
-    res.json(arr);
+    News.find({}).then((result) => res.json(getAns(result, limit, 0)));
 })
 
 app.get('/newsFeeds/:offset', (req, res) => {
     let offset = req.params.offset;
     let arr = [];
     if (isNaN(offset) || Number(offset) >= data.length) {
-        for (let i = 0; i < 10; i++) {
-            arr.push(data[i]);
-        }
-        res.json(arr);
+        News.find({}).then((result) => res.json(getAns(result, 10, 0)));
         return;
     }
     offset = Number(offset);
-    for (let i = offset; i < offset + 10; i++) {
-        arr.push(data[i]);
-    }
-    res.json(arr);
+    News.find({}).then((result) => res.json(getAns(result, 10, offset)));
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
