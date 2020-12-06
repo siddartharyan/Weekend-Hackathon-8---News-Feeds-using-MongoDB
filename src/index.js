eae334512fa1ce5964fdec6f31c55f27cb1aa543
unknown <siddartharyan180@gmail.com>
2020-12-06 20:45:28 +0530

    c1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 8080
-const data = require('./data');
-const len = data.length;
-const onePageArticleCount = 10
 const { newsArticleModel } = require('./connector');
+
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
 app.get('/newFeeds', (req, res) => {
-    const limit = (isNaN(req.query.limit) || Number(req.query.limit) >= len) ? 10 : Number(req.query.limit);
-    const offset = (isNaN(req.query.offset) || Number(req.query.offset) >= len) ? 0 : Number(req.query.offset);
-    newsArticleModel.find({}).skip(offset).limit(limit).then((result) => res.json(result)).then((err) => res.status(400).json(err))
-})
+    let limit = isNaN(req.query.limit) || !req.query.limit ? 10 : parseInt(req.query.limit);
+    let offset = isNaN(req.query.offset) || !req.query.offset ? 0 : parseInt(req.query.offset);
+
+    newsArticleModel.find().limit(limit).skip(offset).then((result) => {
+        res.send(result);
+    });
+});
+
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
-module.exports = app;
+module.exports = app
