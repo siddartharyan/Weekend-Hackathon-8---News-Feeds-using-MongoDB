0f76ef0b3589a4584cd42f29888e71938c2b3bec
unknown <siddartharyan180@gmail.com>
2020-12-06 11:30:58 +0530

    xc

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const len = data.length;
 const onePageArticleCount = 10
 const { newsArticleSchema } = require('./schema')
 const { newsArticleModel } = require('./connector');
-const News = newsArticleModel;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
@@ -14,7 +13,7 @@ app.use(express.json());
 app.get('/newsFeeds', (req, res) => {
     const limit = (isNaN(req.query.limit) || Number(req.query.limit) >= len) ? 10 : Number(req.query.limit);
     const offset = (isNaN(req.query.offset) || Number(req.query.offset) >= len) ? 0 : Number(req.query.offset);
-    News.find({}).skip(offset).limit(limit).then((result) => res.json(result)).then((err) => res.status(400).json(err))
+    newsArticleModel.find({}).skip(offset).limit(limit).then((result) => res.json(result)).then((err) => res.status(400).json(err))
 })
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
